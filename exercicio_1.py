5a84f315be7853e7c2f09f87187dfb748bbe526d
Ramom <213066570+ClementeRamom777@users.noreply.github.com>
2026-08-14 12:18:57 -0300

    Aula 2:exercício 1 - cálculo de desconto com entrada do usuário

---
+++ exercicio_1.py
@@ -0,0 +1,7 @@
+#Desenvolva um algoritmo que solicite ao usuário o preço de um produto e um percentual de desconto a ser aplicado a ele. Calcule e exiba o valor do desconto e o preço final do produto.
+preco = float(input('Digite o preço do produto: '))
+percentual = float(input('Digite o percentual de desconto: (0-100%): '))
+desconto = preco * (percentual/100)
+final = preco - desconto
+print(f'O preco do produto é {preco}. Desconto de {percentual}%')
+print(f'Valor calculado de desconto: {desconto}. Valor final do produto: {final}')
