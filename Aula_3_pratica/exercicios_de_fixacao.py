94cbe1b6959653e90ceb10067a574b38d35a292b
Ramom <213066570+ClementeRamom777@users.noreply.github.com>
2026-08-26 16:29:36 -0300

    chore: padroniza estrutura com um unico ambiente virtual

--- Aula_3_pratica/exercicios_de_fixacao.py
+++ Aula_3_pratica/exercicios_de_fixacao.py
@@ -1,8 +1,8 @@
 #Escreva as seguintes exepressões booleanas em linguagem Python
 
 #a) O somatório de 2 com 2 é menor que 4:
-if (2 + 2 < 4):
-
+if 2 + 2 < 4 :
+    print("Verdadeiro!")
 #b) O valor 7 // 3 é igual a 1 + 1
 
 
