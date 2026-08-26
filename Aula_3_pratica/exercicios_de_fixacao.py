c526fa507897e87d3ae43e7d5761c4de0100671f
Ramom <213066570+ClementeRamom777@users.noreply.github.com>
2026-08-26 16:51:29 -0300

    feat: resolve exercícios de fixação da aula 3

--- Aula_3_pratica/exercicios_de_fixacao.py
+++ Aula_3_pratica/exercicios_de_fixacao.py
@@ -4,5 +4,24 @@
 if 2 + 2 < 4 :
     print("Verdadeiro!")
 #b) O valor 7 // 3 é igual a 1 + 1
+if (7 // 3 == 1 + 1):
+    print("Verdadeiro!")
+#c) A soma de 3 elevado ao quadrado com 4 elevado ao quadrado é igual a 25
+if (3 ** 2 + 4 ** 2 == 25):
+    print("Verdadeiro!")
 
+#d) A soma de 2,4 e 6 é maior que 12
+if (2 + 4 + 6 > 12):
+    print("Verdadeiro!")
 
+#Parte 2
+
+#a) 1387 é dividível por 19
+if (1387 % 19 == 0):
+    print("Verdadeiro!")
+#b) 31 é par
+if (31 % 2 == 0):
+    print("Verdadeiro!")
+#c) O menor valor entre: 34, 29 e 31 é menor que 30
+if (min(34, 29 , 31) < 30):
+    print("Verdadeiro!")
